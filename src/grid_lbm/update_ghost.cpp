95eeb0925c523ac77a9502bf8371b2e82a374972
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2025-02-21 23:38:38 +0100

    add update ghost

---
+++ src/grid_lbm/update_ghost.cpp
@@ -0,0 +1,56 @@
+#include <mpi.h>
+#include <onika/scg/operator.h>
+#include <onika/scg/operator_slot.h>
+#include <onika/scg/operator_factory.h>
+#include <onika/log.h>
+#include <onika/cuda/cuda.h>
+#include <onika/memory/allocator.h>
+#include <onika/parallel/parallel_for.h>
+
+#include <grid_lbm/domain_lbm.hpp>
+#include <grid_lbm/comm.hpp>
+#include <grid_lbm/enum.hpp>
+#include <grid_lbm/grid_data_lbm.hpp>
+#include <grid_lbm/domain_lbm.hpp>
+#include <grid_lbm/update_ghost.hpp>
+
+namespace hippoLBM
+{
+  using namespace onika;
+  using namespace scg;
+  using namespace onika::cuda;
+
+  template<int Q>
+    class UpdateGhost : public OperatorNode
+  {
+      ADD_SLOT( grid_data_lbm<Q>, GridDataQ, INPUT_OUTPUT, REQUIRED, DocString{"Grid data for the LBM simulation, including distribution functions and macroscopic fields."});
+      ADD_SLOT( domain_lbm<Q>, DomainQ, INPUT, REQUIRED);
+
+    public:
+
+      inline std::string documentation() const override final
+      {
+        return R"EOF(  A functor for computing macroscopic variables (densities and flux) for lattice Boltzmann method.
+        )EOF";
+      }
+
+      inline void execute () override final
+      {
+        auto& data = *GridDataQ;
+        auto& domain = *DomainQ;
+
+        // get fields
+        WrapperF<Q> pf = data.distributions();
+        update_ghost(domain, pf);
+      }
+  };
+
+  using UpdateGhost3D19Q = UpdateGhost<19>;
+
+  // === register factories ===  
+  ONIKA_AUTORUN_INIT(update_ghost)
+  {
+    OperatorNodeFactory::instance()->register_factory( "update_ghost", make_compatible_operator<UpdateGhost3D19Q>);
+  }
+}
+
